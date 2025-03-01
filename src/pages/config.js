a47ffb58198ef6a1006076598659bda8b280375b
Shreyas Rao <109731624+Humbledot00@users.noreply.github.com>
2025-03-01 15:59:44 +0530

    Update config.js

--- src/pages/config.js
+++ src/pages/config.js
@@ -1,5 +1,5 @@
 const config = {
-    baseUrl: "http://127.0.0.1:5000", // Base URL for the backend API
+    baseUrl: "https://edusimplify-backend.onrender.com", // Base URL for the backend API
   };
   
   export default config;
